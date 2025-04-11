aa650a99277fe838303e2dfe31a158c6fd647b57
WaadAlharbi <109552640+WaadAlharbi@users.noreply.github.com>
2025-04-11 08:34:56 +0300

    Create step_trainer_landing.sql

---
+++ SQL DDL/step_trainer_landing.sql
@@ -0,0 +1,14 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://spark-and-stedi-human-balance-w1/Step_trainer/Landing/'
+TBLPROPERTIES (
+  'classification'='json')
